014a1c758add4256958dfeff0354e6a162356677
jomolew1 <jomolew1@swarthmore.edu>
2023-05-28 03:44:33 -0400

    final search page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import CountryDatasets from './page';
 import './App.css';
+
+function MainApp() {
+  const location = useLocation();
+  const countryCode = location.pathname.split('/')[3];
+
+  return (
+    <div className="App">
+        <CountryDatasets countryCode={countryCode} />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/geonode/datasets/:countryCode" element={<MainApp />} />
+      </Routes>
+    </Router>
   );
 }
 
